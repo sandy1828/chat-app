efbbd795ec395f45c87717596c54f63d010f2911
sandy1828 <digital.sandeepsanotra@gmail.com>
2025-08-21 13:06:40 +0530

    Update socket.js

--- chat-app/src/socket.js
+++ chat-app/src/socket.js
@@ -3,7 +3,7 @@ import { io } from 'socket.io-client';
 let socket;
 
 export const connectSocket = (token) => {
-  socket = io('http://10.130.84.248:3000', {
+  socket = io('http://10.130.84.248:3000', { //- update with ur wifi ip
     auth: { token },
   });
   return socket;
